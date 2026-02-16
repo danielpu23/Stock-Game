ea90acd868469ea8045826c917d6b0068de2263b
Daniel Pu <daniel.pu@sjsu.edu>
2026-02-16 01:09:49 -0800

    updated simple user backend

---
+++ backend/src/main/java/com/stock/stock_game/StockGameApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stock_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockGameApplication.class, args);
+	}
+
+}
